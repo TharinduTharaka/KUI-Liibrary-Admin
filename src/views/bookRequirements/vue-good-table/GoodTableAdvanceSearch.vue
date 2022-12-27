ae68da3f62d1f5ff6bfc60b1f2228e0bd4125d58
thamindu <thaminduanu2@gmail.com>
2022-12-27 12:15:15 +0530

    Changes

---
+++ src/views/bookRequirements/vue-good-table/GoodTableAdvanceSearch.vue
@@ -0,0 +1,535 @@
+<template>
+
+
+  <b-card>
+    <b-button v-b-toggle.sidebar-creat
+              style="margin-bottom: 10px"
+              variant="primary"
+    >
+      Add
+    </b-button>
+    <b-sidebar
+        id="sidebar-creat"
+        backdrop
+        bg-variant="white"
+        right
+        shadow
+    >
+      <sidebar-content title="Create"/>
+    </b-sidebar>
+    <b-sidebar
+        id="sidebar-edit"
+        backdrop
+        bg-variant="white"
+        right
+        shadow
+    >
+      <sidebar-content title="Edit" :id="this.edit_id" :dep="this.edit_department" :edit_title="this.edit_title" :edit_author="this.edit_author" :edit_resource="this.edit_resource" :edit_document="this.edit_document" :edit_cover="this.edit_cover" :edit_description="this.edit_description" :edit_type="this.edit_type"/>
+    </b-sidebar>
+    <div class="custom-search">
+
+      <!-- advance search input -->
+      <b-row>
+        <b-col md="4">
+          <b-form-group>
+            <label>Title:</label>
+            <b-form-input
+                class="d-inline-block"
+                placeholder="Search"
+                type="text"
+                @input="advanceSearch"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col md="4">
+          <b-form-group>
+            <label>Author:</label>
+            <b-form-input
+                class="d-inline-block"
+                placeholder="Search"
+                type="text"
+                @input="advanceSearch"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col md="4">
+          <b-form-group>
+            <label>Department:</label>
+            <v-select
+                :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"
+                :options="departmentOptions"
+                placeholder="Please select"
+                @input="advanceSearch"
+            />
+            <!--            <b-form-input-->
+            <!--                class="d-inline-block"-->
+            <!--                placeholder="Search"-->
+            <!--                type="text"-->
+            <!--                @input="advanceSearch"-->
+            <!--            />-->
+          </b-form-group>
+        </b-col>
+        <b-col md="4">
+          <b-form-group>
+            <label>Resource:</label>
+            <v-select
+                :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"
+                :options="resourceOptions"
+                placeholder="Please select"
+                @input="advanceSearch"
+            />
+            <!--            <b-form-input-->
+            <!--                class="d-inline-block"-->
+            <!--                placeholder="Search"-->
+            <!--                type="text"-->
+            <!--                @input="advanceSearch"-->
+            <!--            />-->
+          </b-form-group>
+        </b-col>
+        <b-col md="4">
+          <b-form-group>
+            <label>Status:</label>
+            <b-form-input
+                class="d-inline-block"
+                placeholder="Search"
+                type="text"
+                @input="advanceSearch"
+            />
+          </b-form-group>
+        </b-col>
+        <!--        <b-col md="4">-->
+        <!--          <b-form-group>-->
+        <!--            <label>Salary:</label>-->
+        <!--            <b-form-input-->
+        <!--                class="d-inline-block"-->
+        <!--                placeholder="Search"-->
+        <!--                type="text"-->
+        <!--                @input="advanceSearch"-->
+        <!--            />-->
+        <!--          </b-form-group>-->
+        <!--        </b-col>-->
+      </b-row>
+    </div>
+
+    <!-- table -->
+    <div>
+      <b-table
+          :current-page="currentPage"
+          :fields="fields"
+          :filter="filter"
+          :items="items"
+          :per-page="perPage"
+          :sort-by.sync="sortBy"
+          :sort-desc.sync="sortDesc"
+          :sort-direction="sortDirection"
+          class="position-relative"
+          hover
+          responsive
+          striped
+      >
+        <template #cell(show_details)="row">
+
+          <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
+          <b-form-checkbox
+              v-model="row.detailsShowing"
+              @change="row.toggleDetails"
+          >
+            {{ row.detailsShowing ? 'Hide' : 'Show' }}
+          </b-form-checkbox>
+        </template>
+
+        <!-- full detail on click -->
+        <template #row-details="row">
+          <b-card>
+            <b-row class="mb-2">
+              <b-col>
+                <div class="bg-light-primary rounded-top text-center">
+                  <b-img
+                      :src="getImage(items[row.index].document_image)"
+                      height="170"
+
+                  />
+                </div>
+              </b-col>
+            </b-row>
+
+
+            <b-row class="mb-2">
+              <b-col
+                  class="mb-1"
+                  md="4"
+              >
+                <strong>Published Date : </strong>{{ row.item.added_date }}
+              </b-col>
+              <b-col
+                  class="mb-1"
+                  md="4"
+              >
+                <strong>Title : </strong>{{ row.item.title }}
+              </b-col>
+              <b-col
+                  class="mb-1"
+                  md="4"
+              >
+                <strong>Author : </strong>{{ row.item.author }}
+              </b-col>
+              <b-col
+                  class="mb-1"
+                  md="4"
+              >
+                <strong>Department : </strong>{{ row.item.department }}
+              </b-col>
+              <b-col
+                  class="mb-1"
+                  md="4"
+              >
+                <strong>Resource : </strong>{{ row.item.resource }}
+              </b-col>
+              <b-col
+                  class="mb-1"
+                  md="4"
+              >
+                <strong>Status : </strong>{{ row.item.status }}
+              </b-col>
+              <b-col
+                  class="mb-1"
+                  md="4"
+              >
+                <strong>Resource Title : </strong>{{ row.item.resource_title }}
+              </b-col>
+              <b-col
+                  class="mb-1"
+                  md="4"
+              >
+                <strong>Resource Type : </strong>{{ row.item.type }}
+              </b-col>
+              <b-col
+                  class="mb-1"
+                  md="4"
+              >
+                <strong>Description : </strong>{{ items[row.index].description }}
+              </b-col>
+
+              <!--              <b-col-->
+              <!--                  class="mb-1"-->
+              <!--                  md="4"-->
+              <!--              >-->
+              <!--                <strong>Age : </strong>{{ row.item.age }}-->
+              <!--              </b-col>-->
+            </b-row>
+            <div class="demo-inline-spacing">
+              <b-button
+                  size="sm"
+                  variant="outline-secondary"
+                  @click="row.toggleDetails"
+              >
+                Hide Details
+              </b-button>
+              <b-button
+                  v-b-toggle.sidebar-edit
+                  size="sm"
+                  @click="edit(row.item.id,row.item.title, row.item.department, row.item.resource, row.item.author, row.item.resource_title, items[row.index].document_image, items[row.index].description, row.item.type )"
+                  style="margin-left: 10px"
+                  variant="outline-primary"
+              >
+                Edit
+              </b-button>
+              <b-button
+                  size="sm"
+                  @click="deleteResource(row.item.id,5)"
+                  style="margin-left: 10px"
+                  variant="outline-danger"
+              >
+                Delete
+              </b-button>
+              <b-button
+                  size="sm"
+                  style="margin-left: 10px"
+                  variant="outline-success"
+                  @click="updateEResourceStatus(row.item.id,getStatus(row.item.status),5)"
+              >
+                {{ getStatus(row.item.status) }}
+              </b-button>
+            </div>
+          </b-card>
+        </template>
+
+        <template #cell(avatar)="data">
+          <b-avatar :src="data.value"/>
+        </template>
+
+        <template #cell(status)="data">
+          <b-badge :variant="status[1][data.value]">
+            {{ status[0][data.value] }}
+          </b-badge>
+        </template>
+      </b-table>
+    </div>
+    <b-card-body class="d-flex justify-content-between flex-wrap pt-0">
+
+      <!-- page length -->
+      <b-form-group
+          class="text-nowrap mb-md-0 mr-1"
+          label="Per Page"
+          label-align="left"
+          label-cols="6"
+          label-for="sortBySelect"
+          label-size="sm"
+      >
+        <b-form-select
+            id="perPageSelect"
+            v-model="perPage"
+            :options="pageOptions"
+            inline
+            size="sm"
+        />
+      </b-form-group>
+
+      <!-- pagination -->
+      <div>
+        <b-pagination
+            v-model="currentPage"
+            :per-page="perPage"
+            :total-rows="totalRows"
+            class="mb-0"
+            first-number
+            last-number
+            next-class="next-item"
+            prev-class="prev-item"
+        >
+          <template #prev-text>
+            <feather-icon
+                icon="ChevronLeftIcon"
+                size="18"
+            />
+          </template>
+          <template #next-text>
+            <feather-icon
+                icon="ChevronRightIcon"
+                size="18"
+            />
+          </template>
+        </b-pagination>
+      </div>
+    </b-card-body>
+
+    <!--    <template #code>-->
+    <!--      {{ codeAdvance }}-->
+    <!--    </template>-->
+  </b-card>
+</template>
+
+<script>
+// import BCardCode from '@core/components/b-card-code/BCardCode.vue'
+import {
+  BAvatar,
+  BBadge,
+  BButton,
+  BCard,
+  BCardBody,
+  BCol,
+  BFormCheckbox,
+  BFormGroup,
+  BFormInput,
+  BFormSelect,
+  BImg,
+  BPagination,
+  BRow,
+  BSidebar,
+  BTable,
+  VBToggle,
+} from 'bootstrap-vue' // eslint-disable-line
+import { VueGoodTable } from 'vue-good-table'
+import store from '@/store/index'
+import Ripple from 'vue-ripple-directive'
+import SidebarContent from './SidebarContent.vue'
+import vSelect from 'vue-select'
+import axios from "axios";
+// import { codeAdvance } from './code'
+/* eslint-disable */
+export default {
+  components: {
+    BCard,
+    BImg,
+    BCardBody,
+    vSelect,
+    BBadge,
+    BSidebar,
+    SidebarContent,
+    BFormCheckbox,
+    BTable,
+    BButton,
+    VueGoodTable,
+    BAvatar,
+    BPagination,
+    BFormGroup,
+    BFormInput,
+    BFormSelect,
+    BRow,
+    BCol,
+    // eslint-disable-next-line vue/no-unused-components
+    // ToastificationContent,
+  },
+  /* eslint-disable */
+  directives: {
+    'b-toggle': VBToggle,
+    Ripple,
+  },
+  /* eslint-disable */
+  data() {
+    return {
+      pageLength: 5,
+      pageOptions: [3, 5, 10],
+      perPage: 5,
+      totalRows: 10,
+      currentPage: 1,
+      edit_id: '',
+      edit_title: '',
+      edit_department: '',
+      edit_resource: '',
+      edit_author: '',
+      edit_document: '',
+      edit_cover: '',
+      edit_description: '',
+      edit_type: '',
+      sortBy: '',
+      sortDesc: false,
+      sortDirection: 'asc',
+      dir: false,
+      // filter: {
+      //   resource:null,
+      //   department:null,
+      // },
+      filter: null,
+      resource: '',
+      resourceOptions: ['Thesis', 'General'],
+      department: '',
+      departmentOptions: ['Nursing', 'BMS', 'Psychology', 'Management', 'Acupuncture', 'IT'],
+      rows: [],
+      searchTerm: '',
+      fields: [
+        'show_details',
+        'id',
+        'added_date',
+        'resource_title',
+        'type',
+        'title',
+        'author',
+        'department',
+        'resource',
+        {
+          key: 'status',
+          label: 'Status'
+
+        }],
+      /* eslint-disable global-require */
+      items: [
+
+      ],
+      /* eslint-disable global-require */
+      status: [{
+        published: 'Published',
+        2: 'Professional',
+        draft: 'Draft',
+        4: 'Resigned',
+        5: 'Applied',
+      },
+        {
+          published: 'light-primary',
+          2: 'light-success',
+          draft: 'light-danger',
+          4: 'light-warning',
+          5: 'light-info',
+        }],
+    }
+  },
+  /* eslint-disable */
+  computed: {
+    direction() {
+      if (store.state.appConfig.isRTL) {
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        this.dir = true
+        return this.dir
+      }
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      this.dir = false
+      return this.dir
+    },
+  },
+  mounted() {
+    // Set the initial number of items
+
+  },
+  /* eslint-disable */
+  created() {
+
+    fetch("http://localhost:8081/book/get-all-info?page=1&limit=2000&sort=tt")
+        .then(async response => {
+          const data = await response.json();
+          this.items = data.data.items;
+          this.totalRows = data.data.total;
+        })
+        .catch(error => {
+          this.errorMessage = error;
+          console.error("There was an error!", error);
+        })
+
+
+  },
+  methods: {
+    advanceSearch(val) {
+      this.filter = val
+    },
+    edit(val,title, department, resource, author, document, cover, description, type) {
+
+      this.edit_id = val
+      this.edit_title = title
+      this.edit_department = department
+      this.edit_resource = resource
+      this.edit_author = author
+      this.edit_document = document
+      this.edit_cover = cover
+      this.edit_description = description
+      this.edit_type = type
+
+      console.log('gggg')
+      console.log(type)
+    },
+    getStatus(val) {
+      if (val == 'draft')
+        return 'published'
+      else
+        return 'draft'
+    },
+    getImage(val) {
+
+      var url = "C:/Users/Administrator/Desktop/ser/50_1.jpg"
+      console.log(val)
+      return require('C:/Users/Administrator/Desktop/ser/50_1.jpg')
+
+    },
+    deleteResource(data,updated_user) {
+      axios.delete("http://localhost:8081/book/delete-eresource",
+          { params: { data,updated_user }})
+          .then(response => window.location.reload());
+    },
+    updateEResourceStatus(data, status,updated_user) {
+      axios.put("http://localhost:8081/book/update-eresource-status", null,
+          { params: { data, status, updated_user }})
+          .then(response => window.location.reload());
+    },
+    getData() {
+      axios.get("http://localhost:8081/book/get-all-info?page=1&limit=20&sort=tt")
+          .then(response => {
+            const data = response.json();
+            this.items = data.data.items;
+          });
+    },
+    onRowClick(params) {
+      console.log(params)
+    }
+  },
+}
+</script>
+''
+''
---
+++ src/views/bookRequirements/vue-good-table/SidebarContent.vue
@@ -0,0 +1,384 @@
+<template>
+  <div>
+    <div class="text-center">
+      <!-- Name -->
+      <b-card-text class="mt-2 h4 color-inherit text-reset">
+        {{ this.loadPage() }}
+      </b-card-text>
+    </div>
+    <b-card>
+      <b-form @submit.prevent>
+        <b-row>
+
+          <!-- Title -->
+          <b-col cols="12">
+            <b-form-group
+                label="Title"
+                label-for="v-title"
+            >
+              <b-form-input
+                  id="v-title"
+                  v-model="post_values.title"
+                  placeholder="Title"
+              />
+            </b-form-group>
+          </b-col>
+
+          <!-- Department -->
+          <b-col cols="12">
+            <b-form-group
+                label="Department"
+                label-for="v-department"
+            >
+              <v-select
+                  v-model="post_values.department"
+                  :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"
+                  :options="model.departmentOptions"
+                  placeholder="Please select"
+              />
+            </b-form-group>
+          </b-col>
+          <!--          <b-col cols="12">-->
+          <!--            <b-form-group-->
+          <!--                label="Type"-->
+          <!--                label-for="v-type"-->
+          <!--            >-->
+          <!--              <v-select-->
+          <!--                  v-model="post_values.type"-->
+          <!--                  :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"-->
+          <!--                  :options="model.type"-->
+          <!--                  placeholder="Please select"-->
+          <!--              />-->
+          <!--            </b-form-group>-->
+          <!--          </b-col>-->
+
+          <!-- Resource -->
+          <!--          <b-col cols="12">-->
+          <!--            <b-form-group-->
+          <!--                label="Resource"-->
+          <!--                label-for="v-resource"-->
+          <!--            >-->
+          <!--              <v-select-->
+          <!--                  v-model="post_values.resource"-->
+          <!--                  :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'"-->
+          <!--                  :options="model.resourceOptions"-->
+          <!--                  placeholder="Please select"-->
+          <!--              />-->
+          <!--            </b-form-group>-->
+          <!--          </b-col>-->
+
+          <!-- Author -->
+          <!--          <b-col cols="12">-->
+          <!--            <b-form-group-->
+          <!--                label="Author"-->
+          <!--                label-for="v-author">-->
+          <!--              <b-form-input-->
+          <!--                  id="v-author"-->
+          <!--                  v-model="post_values.author"-->
+          <!--                  placeholder="Author"-->
+          <!--              />-->
+          <!--            </b-form-group>-->
+          <!--          </b-col>-->
+
+          <!--          Documents-->
+          <div v-if="this.title == 'Create'">
+            <b-col cols="12">
+              <b-form-group
+                  label="Book Document"
+                  label-for="v-documents">
+                <b-form-file
+
+                    @change="handleFileUpload( $event )"
+                    :placeholder="this.getResourceName()"
+                    drop-placeholder="Drop file here..."
+                />
+              </b-form-group>
+            </b-col>
+          </div>
+
+          <div v-if="this.title != 'Create'">
+            <b-col cols="12">
+              <b-form-group
+                  label="Purchasing Order"
+                  label-for="v-documents">
+                <b-form-file
+                    @change="handleFileUploadCoverPhoto( $event )"
+                    :placeholder="this.getCoverName()"
+                    drop-placeholder="Drop file here..."
+                />
+              </b-form-group>
+            </b-col>
+          </div>
+
+          <b-col cols="12">
+            <b-form-group
+                label="Description"
+                label-for="v-description">
+              <b-form-textarea
+                  id="v-description"
+                  v-model="post_values.description"
+                  placeholder="Description"
+              />
+            </b-form-group>
+          </b-col>
+
+          <!-- checkbox -->
+          <!--          <b-col cols="12">-->
+          <!--            <b-form-group>-->
+          <!--              <b-form-checkbox-->
+          <!--                  id="checkbox-3"-->
+          <!--                  name="checkbox-3"-->
+          <!--                  value="Remember_me"-->
+          <!--              >-->
+          <!--                Remember me-->
+          <!--              </b-form-checkbox>-->
+          <!--            </b-form-group>-->
+          <!--          </b-col>-->
+
+          <!-- submit and reset -->
+          <b-col cols="12">
+            <b-button
+                class="mr-1"
+                size="sm"
+                type="submit"
+                @click="submit"
+                variant="primary"
+            >
+              Submit
+            </b-button>
+            <b-button
+                size="sm"
+                type="reset"
+                variant="outline-secondary"
+            >
+              Reset
+            </b-button>
+          </b-col>
+        </b-row>
+      </b-form>
+    </b-card>
+
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import vSelect from 'vue-select'
+import BCardCode from '@core/components/b-card-code/BCardCode.vue'
+import { BCardText, BButton } from 'bootstrap-vue'
+import Ripple from 'vue-ripple-directive'
+import 'animate.css'
+import { codeAnimation } from './code'
+import {
+  BAvatar, BFormTextarea,
+  BCard,
+  BForm,
+  BFormCheckbox,
+  BFormGroup,
+  BFormInput,
+  BListGroup, BFormFile,
+  BListGroupItem,
+} from 'bootstrap-vue'
+import axios from "axios";
+/* eslint-disable */
+export default {
+  props: ['title', 'id', 'dep', 'edit_title', 'edit_author', 'edit_resource', 'edit_document', 'edit_cover', 'edit_description', 'edit_type'],
+  components: {
+    BAvatar, BFormTextarea,
+    BButton,
+    BFormGroup,
+    BFormInput,
+    BFormCheckbox,
+    BForm, BFormFile,
+    BListGroup,
+    BCardCode,
+    BListGroupItem,
+    BCardText,
+    BCard,
+    vSelect,
+  },
+  /* eslint-disable */
+  data() {
+    return {
+      id_back: '',
+      doc: {
+        resource_name: '',
+        cover_name: ''
+      },
+      post_values: {
+        author: '',
+        department: '',
+        title: '',
+        resource: '',
+        type: '',
+        description: ''
+      },
+      model: {
+        file: '',
+        coverPhoto: '',
+        resource: '',
+        department: '',
+        departmentOptions: ['Nursing', 'BMS', 'Psychology', 'Marketing', 'Acupuncture', 'IT', 'HR', 'Accounting'],
+        type: ['Book', 'Journal', 'Magazine', 'PDF', 'Article'],
+        resourceOptions: ['Thesis', 'General'],
+        option: [{title: 'Square'}, {title: 'Rectangle'}, {title: 'Rombo'}, {title: 'Romboid'}],
+      }
+    }
+  },
+  created() {
+    console.log("wsfwfdwfscs")
+  },
+  methods: {
+    loadPage() {
+
+
+      if (this.id_back === '') {
+        this.id_back = this.id
+      } else {
+        if (!(this.id_back === this.id)) {
+          this.post_values.department = ''
+          this.post_values.title = ''
+          this.post_values.author = ''
+          this.post_values.resource = ''
+          this.post_values.type = ''
+          this.doc.resource_name = ''
+          this.doc.description = ''
+        }
+      }
+      this.id_back = this.id
+
+      if (this.post_values.department === '') {
+        this.post_values.department = this.dep
+      } else {
+        this.post_values.department = this.post_values.department
+      }
+      if (this.post_values.type === '') {
+        this.post_values.type = this.edit_type
+      } else {
+        this.post_values.type = this.post_values.type
+      }
+      if (this.post_values.title === '') {
+        this.post_values.title = this.edit_title
+      } else {
+        this.post_values.title = this.post_values.title
+      }
+      if (this.post_values.author === '') {
+        this.post_values.author = this.edit_author
+      } else {
+        this.post_values.author = this.post_values.author
+      }
+      if (this.post_values.resource === '') {
+        this.post_values.resource = this.edit_resource
+      } else {
+        this.post_values.resource = this.post_values.resource
+      }
+      if (this.post_values.description === '') {
+        this.post_values.description = this.edit_description
+      } else {
+        this.post_values.description = this.post_values.description
+      }
+      return this.title
+    },
+
+    submitFinalBookList(){
+
+      var id = response.data.id
+
+      let formData = new FormData();
+      formData.append('files', this.model.file);
+
+
+      axios.create({
+        baseURL: 'http://localhost:8081/book'
+      }).post('/uploadMultipleFiles',
+          formData, {params: {id}},
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }
+      ).then(function () {
+        window.location.reload();
+      })
+          .catch(function () {
+            console.log('FAILURE!!');
+          });
+
+    },
+    submit() {
+
+      this.flipIn()
+
+      if (!(this.title === 'Edit')) {
+        axios.post("http://localhost:8081/book/save-eresource",
+            this.post_values)
+            .then(response => {
+
+              this.submitFile(response, '1')
+
+            });
+      } else {
+
+        var new_id = this.id
+
+        axios.post("http://localhost:8081/book/update-eresource",
+            this.post_values, {params: {new_id}})
+            .then(response => {
+              window.location.reload();
+            });
+      }
+
+    },
+    getResourceName() {
+      return this.edit_document
+    },
+    getCoverName() {
+      return this.edit_cover
+    },
+    handleFileUpload(event) {
+      this.model.file = event.target.files[0];
+    },
+    handleFileUploadCoverPhoto(event) {
+      this.model.coverPhoto = event.target.files[0];
+    },
+
+    flipIn() {
+      this.$swal({
+        title: 'Please wait uploading the document',
+        customClass: {
+          confirmButton: 'btn btn-primary',
+        },
+        showClass: {
+          popup: 'animate__animated animate__flipInX',
+        },
+        buttonsStyling: false,
+      })
+    },
+
+    submitFile(response, type) {
+
+      var id = response.data.id
+
+      let formData = new FormData();
+      formData.append('files', this.model.file);
+
+
+      axios.create({
+        baseURL: 'http://localhost:8081/book'
+      }).post('/uploadMultipleFiles',
+          formData, {params: {id, type}},
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }
+      ).then(function () {
+        window.location.reload();
+      })
+          .catch(function () {
+            console.log('FAILURE!!');
+          });
+    }
+  },
+}
+</script>
